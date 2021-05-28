5d48af1453bd0dc1b4ac0763d91b9516c9bf2414
lhmrosso <lhmrosso@gmail.com>
2021-05-28 17:08:25 -0300

    Data example

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Example locations in Kansas
+#'
+#' @description This locations example was created to help users 
+#' setup their own location data. The locations “Scandia” and 
+#' “Belleville” are close in space, to ilustrate that the code 
+#' only downloads a minimum number of images to extract the soil 
+#' information (proximal locations may fall in the same raster ﬁle).
+#'
+#' @format This data frame has 3 rows and the following 3 columns:
+#' \describe{
+#'   \item{ID}{Unique identifier of a given location.}
+#'   \item{lat}{Latitude coordinate, in decimal degrees.}
+#'   \item{long}{Longitude coordinate, in decimal degrees.}
+#' }
+"exkansas"

a71a64e09fd8a7e293776e39cf95dce20366f0d5
lhmrosso <lhmrosso@gmail.com>
2021-06-04 07:44:39 -0300

    Tests

--- tests/testthat/test-XPolaris.R
+++ tests/testthat/test-XPolaris.R
@@ -1,5 +1,22 @@
 data('exkansas')
+
 plt <- xplot(exkansas)
-test_that('no error in generating the plot', {
+
+img <- ximages(locations = exkansas,
+               variables = c('clay'),
+               statistics = c('mean'),
+               layersdepths = c('0_5'))
+
+soi <- xsoil(img)
+
+test_that('no error plotting', {
   expect_false(inherits(plt, "try-error"))
+})
+
+test_that('no error downloading images', {
+  expect_false(inherits(img, "try-error"))
+})
+
+test_that('no error extracting soil', {
+  expect_false(inherits(soi, "try-error"))
 })

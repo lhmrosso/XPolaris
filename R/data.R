17cdca7e861ab11ec47294badd0e1b681d1fd18c
lhmrosso <lhmrosso@gmail.com>
2021-06-05 15:56:23 -0300

    Checks

--- R/data.R
+++ R/data.R
@@ -1,10 +1,6 @@
 #' Example locations in Kansas
 #'
-#' @description This locations example was created to help users 
-#' setup their own location data. The locations “Scandia” and 
-#' “Belleville” are close in space, to ilustrate that the code 
-#' only downloads a minimum number of images to extract the soil 
-#' information (proximal locations may fall in the same raster ﬁle).
+#' @description This locations example was created to help users setup their own location data. The locations Scandia and Belleville are close in space, to ilustrate that the code only downloads a minimum number of images to extract the soil information (proximal locations may fall in the same raster).
 #'
 #' @format This data frame has 3 rows and the following 3 columns:
 #' \describe{
